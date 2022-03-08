066fc885f3ccf93397b6d4ac44344f8f8c721c54
ntminh6 <47615763+minhnt303@users.noreply.github.com>
2022-03-08 18:29:01 +0700

    commit

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ window.addEventListener('resize', onWindowResize);
 
 // SCENE
 const scene: THREE.Scene = new THREE.Scene()
-scene.background = new THREE.Color(0xbfd1e5);
+scene.background = new THREE.Color('#cccccc');
 
 // CONTROLS
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -56,31 +56,103 @@ function createFloor() {
   let scale = { x: 100, y: 2, z: 100 };
 
   let blockPlane = new THREE.Mesh(new THREE.BoxBufferGeometry(),
-       new THREE.MeshPhongMaterial({ color: 0xf9c834 }));
+       new THREE.MeshPhongMaterial({ color: '#4d4d4d' }));
   blockPlane.position.set(pos.x, pos.y, pos.z);
   blockPlane.scale.set(scale.x, scale.y, scale.z);
   blockPlane.castShadow = true;
   blockPlane.receiveShadow = true;
   scene.add(blockPlane);
+  var grid = new THREE.GridHelper(scale.x, 10);
+  grid.position.set(pos.x, pos.y+1, pos.z);
+  scene.add(grid);
 
   blockPlane.userData.ground = true
 }
 
-// box
-function createBox() {
-  let scale = { x: 6, y: 6, z: 6 }
+function createRoomFloor(xAxis, yAxis, zAxis, depth){
+  let scale = { x: 10, y: 1, z: 10 }
   let pos = { x: 15, y: scale.y / 2, z: 15 }
-
   let box = new THREE.Mesh(new THREE.BoxBufferGeometry(), 
       new THREE.MeshPhongMaterial({ color: 0xDC143C }));
-  box.position.set(pos.x, pos.y, pos.z);
-  box.scale.set(scale.x, scale.y, scale.z);
-  box.castShadow = true;
-  box.receiveShadow = true;
-  scene.add(box)
+      box.position.set(pos.x, pos.y, pos.z);
+      box.scale.set(scale.x, scale.y, scale.z);
+      box.castShadow = true;
+      box.receiveShadow = true;
+      box.userData.draggable = true
+      box.userData.name = 'BOX'
+      scene.add(box)
 
-  box.userData.draggable = true
-  box.userData.name = 'BOX'
+      let box2 = new THREE.Mesh(new THREE.BoxBufferGeometry(), 
+      new THREE.MeshPhongMaterial({ color: 0xDC143C }));
+      box2.position.set(pos.x +10, pos.y+10, pos.z+10);
+      box2.scale.set(10, 10, 10);
+      box2.castShadow = true;
+      box2.receiveShadow = true;
+      box2.userData.draggable = true
+      box2.userData.name = 'BOX'
+      scene.add(box2)
+      // box.add(box2)
+
+
+
+
+      // //Create a frame shape..
+      // var frame = new THREE.Shape();
+      // frame.moveTo( xAxis, -yAxis );
+      // frame.lineTo( xAxis, yAxis );
+      // frame.lineTo( -xAxis, yAxis );
+      // frame.lineTo( -xAxis, -yAxis );
+
+      // // var wall1 = createRoomWall(1,1,1,1,frame);
+      // // frame.add(wall1);
+
+      // //Extrude the shape into a geometry, and create a mesh from it:
+      // var extrudeSettings = {
+      //   steps: 1,
+      //   depth: depth,
+      //   bevelEnabled: false,
+      // };
+      // var geom = new THREE.ExtrudeGeometry(frame, extrudeSettings);
+      // var mesh = new THREE.Mesh(geom, new THREE.MeshPhongMaterial({ color: 0xDC143C }));
+
+      // mesh.castShadow = true;
+      // mesh.receiveShadow = true;
+      // mesh.userData.draggable = true
+      // mesh.userData.name = 'BOX'
+      // // mesh.position.set(pos.x +10, pos.y+10, pos.z+10);
+      // // scene.add(mesh);
+      // box.add(mesh);
+}
+function createRoomWall(xAxis, yAxis, zAxis, depth,frame){
+      var hole = new THREE.Path();
+      hole.moveTo(-3, -2);
+      hole.lineTo( 3, -2);
+      hole.lineTo( 3,  2);
+      hole.lineTo(-3,  2);
+      return hole
+}
+
+// box
+function createBox() {
+  createRoomFloor(0.5, 1, 1, 1)
+  // let scale = { x: 10, y: 1, z: 10 }
+  // let pos = { x: 15, y: scale.y / 2, z: 15 }
+
+  // let box = new THREE.Mesh(new THREE.BoxBufferGeometry(), 
+  //     new THREE.MeshPhongMaterial({ color: 0xDC143C }));
+  // box.position.set(pos.x, pos.y, pos.z);
+  // box.scale.set(scale.x, scale.y, scale.z);
+  // box.castShadow = true;
+  // box.receiveShadow = true;
+  // scene.add(box)
+
+  // var box2 = box;
+  // box2.position.set(pos.x+10, pos.y, pos.z+10);
+  // box2.scale.set(scale.x+10, scale.y + 10, scale.z);
+  // box.add(box2)
+
+  // box.userData.draggable = true
+  // box.userData.name = 'BOX'
 }
 
 function createSphere() {
@@ -137,6 +209,35 @@ function createCastle () {
   })
 }
 
+function createHighLightOnFloor(){
+  console.log(draggable)
+  if(draggable != null){
+    let scale = { x: draggable.scale.x, y: draggable.scale.y, z: draggable.scale.z }
+    let pos = { x: draggable.position.x, y: 0.5, z:  draggable.position.z }
+    let box = new THREE.Mesh(new THREE.BoxBufferGeometry(), 
+        new THREE.MeshPhongMaterial({ color: 0xDC143C }));
+        box.position.set(pos.x, pos.y, pos.z);
+        box.scale.set(scale.x, scale.y, scale.z);
+        box.castShadow = true;
+        box.receiveShadow = true;
+        box.userData.draggable = true
+        box.userData.name = 'BOX2'
+        scene.add(box)
+
+        //create a blue LineBasicMaterial
+        const material = new THREE.LineBasicMaterial( { color: 0x0000ff } )
+        const points = [];
+        points.push( new THREE.Vector3( draggable.position.x + draggable.scale.x/2, 0, draggable.position.z + draggable.scale.z/2 ) );
+        points.push( new THREE.Vector3( 0, 0, draggable.position.z + draggable.scale.z/2 ) );
+        // points.push( new THREE.Vector3( 10, 0, 10) );
+
+        const geometry = new THREE.BufferGeometry().setFromPoints( points );
+        const line = new THREE.Line( geometry, material );
+        draggable.add( line );
+
+  }
+}
+
 const raycaster = new THREE.Raycaster(); // create once
 const clickMouse = new THREE.Vector2();  // create once
 const moveMouse = new THREE.Vector2();   // create once
@@ -150,6 +251,7 @@ function intersect(pos: THREE.Vector2) {
 window.addEventListener('click', event => {
   if (draggable != null) {
     console.log(`dropping draggable ${draggable.userData.name}`)
+    draggable.position.y = 0.5;
     draggable = null as any
     return;
   }
@@ -163,6 +265,9 @@ window.addEventListener('click', event => {
     if (found[0].object.userData.draggable) {
       draggable = found[0].object
       console.log(`found draggable ${draggable.userData.name}`)
+      draggable.position.y = 10;
+      console.log(draggable);
+      createHighLightOnFloor();
     }
   }
 })
@@ -191,8 +296,8 @@ function dragObject() {
 
 createFloor()
 createBox()
-createSphere()
-createCylinder()
-createCastle()
+// createSphere()
+// createCylinder()
+// createCastle()
 
 animate()
